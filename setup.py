10719d2088e50d4c67ae41f641bcfb785713f732
Abdul Rehman <arehmandev@outlook.com>
2017-05-02 12:39:53 +0100

    done

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='jinja2_precommit_hook',
+    description='Some out-of-the-box hooks for pre-commit.',
+    url='https://github.com/arehmandev/jinja2-lint/pre-commit-hooks',
+    version='1.0',
+
+    author='Abdul Rehman',
+    author_email='arehmandev@outlook.com',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'j2-lint = pre_commit_hooks.j2-lint:main',
+        ],
+    },
+)
